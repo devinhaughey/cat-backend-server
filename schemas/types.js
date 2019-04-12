abd4ca24eed8ac07be0985fea8f3daa66716c71c
VerruckterSchlumpfe <verruckterschlumpfe@localhost.localdomain>
2019-04-12 03:13:06 +0300

    Added comments

--- schemas/types.js
+++ schemas/types.js
@@ -1,6 +1,7 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 
+// Defining the shape of the types, matching the shape of the database table 
 const BreedType = new GraphQLObjectType({
   name: "Breed",
   type: "Query",
@@ -18,14 +19,10 @@ const FavoriteType = new GraphQLObjectType({
   name: "Favorites",
   type: "Query",
   fields: {
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-    description: { type: GraphQLString },
-    imageurl: { type: GraphQLString },
-    temperament: { type: GraphQLString },
-    origin: { type: GraphQLString }
+    breedid: { type: GraphQLString }
   }
 });
 
+// Exporting them so they can be used in other places that import them.
 exports.BreedType = BreedType;
 exports.FavoriteType = FavoriteType;

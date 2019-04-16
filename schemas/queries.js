3ab12ddacf70006498828d752375bff6d21e13b9
VerruckterSchlumpfe <verruckterschlumpfe@localhost.localdomain>
2019-04-16 19:51:53 +0300

    Updated for consistency of style

--- schemas/queries.js
+++ schemas/queries.js
@@ -1,109 +1,108 @@
 // Declaring the Database as db to use for queries
-const { db } = require('../dbAdaptor');
-const { 
-        GraphQLObjectType, 
-        GraphQLID, 
-        GraphQLList,
-        GraphQLString 
-        } = require('graphql');
-// Importing the types to use their declared shape for output        
-const { BreedType, FavoriteType } = require('./types');
+const {db} = require("../dbAdaptor");
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLList,
+    GraphQLString
+} = require("graphql");
+// Importing the types to use their declared shape for output
+const {BreedType, FavoriteType} = require("./types");
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  type: 'Query',
-  fields: {
-    getAllBreeds: {
-      type: new GraphQLList(BreedType),
-      description: 'Query to get all cat_schema.breed_table from the database',
-      resolve(parentValue) {
-        const query = `SELECT * FROM cat_schema.breed_table`;
+    name: "RootQueryType",
+    type: "Query",
+    fields: {
+        getAllBreeds: {
+            type: new GraphQLList(BreedType),
+            description: "Query to get all cat_schema.breed_table from the database",
+            resolve(parentValue) {
+                const query = `SELECT * FROM cat_schema.breed_table`;
 
-        return db
-          .query(query)
-          .then(res => res)
-          .catch(err => err);
-      }
-    },
-    getBreedId: {
-      type: BreedType,
-      args: { id: { type: GraphQLID } },
-      description: 'Queries for the provided ID.',
-      resolve(parentValue, args) {
-        const query = `SELECT * FROM cat_schema.breed_table WHERE id=$1`;
-        const values = [args.id];
+                return db
+                    .query(query)
+                    .then(res => res)
+                    .catch(err => err);
+            }
+        },
+        getBreedId: {
+            type: BreedType,
+            args: {id: {type: GraphQLID}},
+            description: "Queries for the provided ID.",
+            resolve(parentValue, args) {
+                const query = `SELECT * FROM cat_schema.breed_table WHERE id=$1`;
+                const values = [args.id];
 
-        return db
-          .one(query, values)
-          .then(res => res)
-          .catch(err => err);
-      }
-    },
-    getBreedByName: {
-        type: BreedType,
-        args: { name: { type: GraphQLString }},
-        description: 'Queries for the breed by name',
-        resolve(parentValue, args) {
-          const query = `SELECT * FROM cat_schema.breed_table WHERE name=$1`;
-          const values = [args.name];
-  
-          return db
-            .one(query, values)
-            .then(res => res)
-            .catch(err => err);
-        }
-    },
-    getPaginateOfBreeds: {
-        type: new GraphQLList(BreedType),
-        args: { size: { type: GraphQLID }},
-        description: 'Queries for all of the breeds but filters the top number of rows.',
-        resolve(parentValue, args) {
-          const query = `SELECT * FROM cat_schema.breed_table fetch first $1 rows only`;
-          const values = [args.size];
+                return db
+                    .one(query, values)
+                    .then(res => res)
+                    .catch(err => err);
+            }
+        },
+        getBreedByName: {
+            type: BreedType,
+            args: {name: {type: GraphQLString}},
+            description: "Queries for the breed by name",
+            resolve(parentValue, args) {
+                const query = `SELECT * FROM cat_schema.breed_table WHERE name=$1`;
+                const values = [args.name];
 
-          return db
-            .query(query,values)
-            .then(res => res)
-            .catch(err => err);
-        }
-    } ,
-    getFavoriteBreed: {
-        type: BreedType,
-        args: { id: { type: GraphQLID } },
-        description: 'Queries for the favorite breed by the provided Id from the favorite table',
-        resolve(parentValue, args) {
-            const query = `SELECT * FROM cat_schema.favorite_table WHERE breedid=$1`;
-            const values = [args.id];
+                return db
+                    .one(query, values)
+                    .then(res => res)
+                    .catch(err => err);
+            }
+        },
+        getPaginateOfBreeds: {
+            type: new GraphQLList(BreedType),
+            args: {size: {type: GraphQLID}},
+            description: "Queries for all of the breeds but filters the top number of rows.",
+            resolve(parentValue, args) {
+                const query = `SELECT * FROM cat_schema.breed_table fetch first $1 rows only`;
+                const values = [args.size];
 
-            return db
-                .one(query, values)
-                .then(res =>
-                {
-                    // Queries the breed table to get the favorite breed information
-                    const query = `SELECT * FROM cat_schema.breed_table WHERE id=$1`;
-                    const values = [res.breedid];
-                    
-                    return db
-                        .one(query, values)
-                        .then(res => res)
-                        .catch(err => err);
-                })
-                .catch(err => err);
+                return db
+                    .query(query, values)
+                    .then(res => res)
+                    .catch(err => err);
+            }
+        },
+        getFavoriteBreed: {
+            type: BreedType,
+            args: {id: {type: GraphQLID}},
+            description: "Queries for the favorite breed by the provided Id from the favorite table",
+            resolve(parentValue, args) {
+                const query = `SELECT * FROM cat_schema.favorite_table WHERE breedid=$1`;
+                const values = [args.id];
+
+                return db
+                    .one(query, values)
+                    .then(res => {
+                        // Queries the breed table to get the favorite breed information
+                        const query = `SELECT * FROM cat_schema.breed_table WHERE id=$1`;
+                        const values = [res.breedid];
+
+                        return db
+                            .one(query, values)
+                            .then(res => res)
+                            .catch(err => err);
+                    })
+                    .catch(err => err);
+            }
+        },
+        getAllfavorites: {
+            type: new GraphQLList(FavoriteType),
+            description: "Queries for all of the favorite breed Ids",
+            resolve(parentValue) {
+                const query = `SELECT * FROM cat_schema.favorite_table`;
+
+                return db
+                    .query(query)
+                    .then(res => res)
+                    .catch(err => err);
+            }
         }
-    },
-    getAllfavorites: {
-        type: new GraphQLList(FavoriteType),
-        description: 'Queries for all of the favorite breed Ids',
-        resolve(parentValue) {
-          const query = `SELECT * FROM cat_schema.favorite_table`;
-  
-          return db
-            .query(query)
-            .then(res => res)
-            .catch(err => err);
-        }
-    }  
-  }
+    }
 });
 
 // Exports the queries so that GraphQL can use them when imported.

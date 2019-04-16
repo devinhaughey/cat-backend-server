e942b815c687a057c86d9880684459fd54e3a410
VerruckterSchlumpfe <verruckterschlumpfe@localhost.localdomain>
2019-04-16 23:00:07 +0300

    Added unit tests for mutations

---
+++ test/mutations.test.js
@@ -0,0 +1,42 @@
+const chai = require("chai");
+
+const expect = chai.expect;
+const url = "http://localhost:5000";
+const request = require("supertest");
+
+describe("Mutations", () => {
+    it("Adds new favorite ID with id = 12", (done) => {
+        const expected = {
+            "breed_id": "12",
+        };
+        const query = "mutation{ addFavorite(id: 12)}";
+
+        request(url)
+            .post("/graphql")
+            .send({query: "mutation{ addFavorite(id: 12) {breed_id}}"})
+            .expect(200)
+            .end((err, res) => {
+                // res returns the breed_id that was added to the table
+                if (err) return done(err);
+                expect(res.body.data.addFavorite).to.deep.equal(expected);
+                done();
+            })
+    });
+
+    it("Deletes new favorite ID with id = 12", (done) => {
+        const expected = {
+            "breed_id": "12",
+        };
+
+        request(url)
+            .post("/graphql")
+            .send({query: "mutation{ deleteFavorite(id: 12) {breed_id}}"})
+            .expect(200)
+            .end((err, res) => {
+                // res returns the breed_id of the deleted breed
+                if (err) return done(err);
+                expect(res.body.data.deleteFavorite).to.deep.equal(expected);
+                done();
+            })
+    })
+});
